96c6f36a555a33624ee383c211531ae7b0aa261a
Klim <naiberd669@bk.ru>
2022-10-24 18:00:58 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetNumberShowSecondNumber (int numberfunction)
+{
+    numberfunction=numberfunction/10;
+    numberfunction=numberfunction%10;
+    return numberfunction;
+}
+
+Console.Write("Введите 3-х значное число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+int result=GetNumberShowSecondNumber(number);
+Console.WriteLine(result);
